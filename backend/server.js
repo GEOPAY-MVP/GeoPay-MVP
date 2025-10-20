7874ef859575030a40411e7c0e43d24d48684904
NoOne619 <abdullahmehmood454@gmail.com>
2025-10-20 23:17:57 +0500

    api name changed

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import transactionRoutes from "./routes/transaction.routes.js";
 const app = express();
 app.use(express.json());
 
-app.use("/api/transactions", transactionRoutes);
+app.use("/wallet/transactions", transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
